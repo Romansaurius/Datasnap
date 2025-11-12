55b84684604bdd6081276c7a477659515921f87d
saantydev <saantydev@gmail.com>
2025-11-12 00:57:08 -0300

    Update __init__.py

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -1,1 +1,4 @@
-# Optimizers package - IA PERFECTA para DataSnap
+# Optimizers package - IA PERFECTA para DataSnap# Optimizers package - IA PERFECTA para DataSnap
+from .advanced_csv_optimizer import AdvancedCSVOptimizer
+from .fixed_dynamic_sql_optimizer import FixedDynamicSQLOptimizer
+from .advanced_xlsx_optimizer import AdvancedXLSXOptimizer
